b7d6ebc767c78a4d4a684c3e0103a9151a1c6ccb
lingyu <Linhuantan@weeget.cn>
2022-01-30 22:12:46 +0800

    commit

--- flutter_libs/lib/widget/img_widget.dart
+++ flutter_libs/lib/widget/img_widget.dart
@@ -1,6 +1,4 @@
-import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_libs/widget/loading_widget.dart';
 
 class ImgWidget extends StatelessWidget {
   final String url;
@@ -8,21 +6,6 @@ class ImgWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ExtendedImage.network(
-      url,
-      fit: BoxFit.fill,
-      cache: true,
-      border: Border.all(color: Colors.red, width: 1.0),
-      borderRadius: const BorderRadius.all(Radius.circular(30.0)),
-      loadStateChanged: (ExtendedImageState stauts) {
-        switch (stauts.extendedImageLoadState) {
-          case LoadState.loading:
-            return const LoadingWidget();
-          case LoadState.failed:
-            return const Text('加载失败');
-          case LoadState.completed:
-        }
-      },
-    );
+    return Image.network(url);
   }
 }
